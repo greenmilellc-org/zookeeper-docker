7e2a74faa742696c4237247af3765471535455be
Kaynan Coelho Lira <kaynancl.kc@gmail.com>
2020-05-29 19:09:22 -0300

    fix docker login

--- 3.6.1/build.sh
+++ 3.6.1/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "[Build] Authenticating with ECR"
 if grep -lq '\[mfa\]' ~/.aws/credentials; then
-  $(aws ecr get-login-password --region us-east-1 --profile mfa | docker login --username AWS --password-stdin 416142281294.dkr.ecr.us-east-1.amazonaws.com) || { echo 'ECR authentication failed' ; exit 1; }
+  $(aws ecr get-login --region us-east-1 --no-include-email --profile mfa) || { echo 'ECR authentication failed' ; exit 1; }
 else
-  $(aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 416142281294.dkr.ecr.us-east-1.amazonaws.com) || { echo 'ECR authentication failed' ; exit 1; }
+  $(aws ecr get-login --region us-east-1 --no-include-email) || { echo 'ECR authentication failed' ; exit 1; }
 fi
 docker build -t gm-zookeeper .
 docker tag gm-zookeeper:latest 416142281294.dkr.ecr.us-east-1.amazonaws.com/gm-zookeeper:3.6.1
