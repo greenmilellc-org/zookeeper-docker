b927c569185662cc57aecd5b1dc8f1d79bfc2e97
Kaynan Coelho Lira <kaynancl.kc@gmail.com>
2020-05-29 18:09:56 -0300

    Adding compatibility with aws service discovery

--- 3.6.1/docker-entrypoint.sh
+++ 3.6.1/docker-entrypoint.sh
@@ -3,45 +3,65 @@
 set -e
 
 # Allow the container to be started with `--user`
-if [[ "$1" = 'zkServer.sh' && "$(id -u)" = '0' ]]; then
-    chown -R zookeeper "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR" "$ZOO_CONF_DIR"
-    exec gosu zookeeper "$0" "$@"
+if [[ "$1" == 'zkServer.sh' && "$(id -u)" == '0' ]]; then
+  chown -R zookeeper "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR" "$ZOO_CONF_DIR"
+  exec gosu zookeeper "$0" "$@"
 fi
 
 # Generate the config only if it doesn't exist
 if [[ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]]; then
-    CONFIG="$ZOO_CONF_DIR/zoo.cfg"
-    {
-        echo "dataDir=$ZOO_DATA_DIR" 
-        echo "dataLogDir=$ZOO_DATA_LOG_DIR"
+  CONFIG="$ZOO_CONF_DIR/zoo.cfg"
+  {
+    echo "dataDir=$ZOO_DATA_DIR"
+    echo "dataLogDir=$ZOO_DATA_LOG_DIR"
 
-        echo "tickTime=$ZOO_TICK_TIME"
-        echo "initLimit=$ZOO_INIT_LIMIT"
-        echo "syncLimit=$ZOO_SYNC_LIMIT"
+    echo "tickTime=$ZOO_TICK_TIME"
+    echo "initLimit=$ZOO_INIT_LIMIT"
+    echo "syncLimit=$ZOO_SYNC_LIMIT"
 
-        echo "autopurge.snapRetainCount=$ZOO_AUTOPURGE_SNAPRETAINCOUNT"
-        echo "autopurge.purgeInterval=$ZOO_AUTOPURGE_PURGEINTERVAL"
-        echo "maxClientCnxns=$ZOO_MAX_CLIENT_CNXNS"
-        echo "standaloneEnabled=$ZOO_STANDALONE_ENABLED"
-        echo "admin.enableServer=$ZOO_ADMINSERVER_ENABLED"
-    } >> "$CONFIG"
-    if [[ -z $ZOO_SERVERS ]]; then
-      ZOO_SERVERS="server.1=localhost:2888:3888;2181"
-    fi
+    echo "autopurge.snapRetainCount=$ZOO_AUTOPURGE_SNAPRETAINCOUNT"
+    echo "autopurge.purgeInterval=$ZOO_AUTOPURGE_PURGEINTERVAL"
+    echo "maxClientCnxns=$ZOO_MAX_CLIENT_CNXNS"
+    echo "standaloneEnabled=$ZOO_STANDALONE_ENABLED"
+    echo "admin.enableServer=$ZOO_ADMINSERVER_ENABLED"
+  } >>"$CONFIG"
+  if [[ -z $ZOO_SERVERS ]]; then
+    ZOO_SERVERS="server.1=localhost:2888:3888;2181"
+  fi
 
-    for server in $ZOO_SERVERS; do
-        echo "$server" >> "$CONFIG"
-    done
+  for server in $ZOO_SERVERS; do
+    echo "$server" >>"$CONFIG"
+  done
 
-    if [[ -n $ZOO_4LW_COMMANDS_WHITELIST ]]; then
-        echo "4lw.commands.whitelist=$ZOO_4LW_COMMANDS_WHITELIST" >> "$CONFIG"
-    fi
+  if [[ -n $ZOO_4LW_COMMANDS_WHITELIST ]]; then
+    echo "4lw.commands.whitelist=$ZOO_4LW_COMMANDS_WHITELIST" >>"$CONFIG"
+  fi
 
 fi
 
 # Write myid only if it doesn't exist
 if [[ ! -f "$ZOO_DATA_DIR/myid" ]]; then
-    echo "${ZOO_MY_ID:-1}" > "$ZOO_DATA_DIR/myid"
+  echo "${ZOO_MY_ID:-1}" >"$ZOO_DATA_DIR/myid"
+fi
+
+# Waiting for DNS resolution from all nodes.
+if [[ -z "$ZOOKEEPER_ENV" ]]; then
+  echo "Please define ZOOKEEPER_ENV."
+else
+  echo "Waiting for at least $DESIRED_COUNT instances with unique IPs to be registered as zookeeper.${ZOOKEEPER_ENV} on Route 53."
+  for ((i = 0; i < ${DESIRED_COUNT}; i++)); do
+    while [ -z "$SERVER" ]; do
+      SERVER=$(dig +short zk0$((i + 1)).zookeeper.${ZOOKEEPER_ENV})
+      if [ -z "$SERVER" ]; then
+        echo "zk0$((i + 1)).zookeeper.${ZOOKEEPER_ENV} not found"
+        echo "Waiting 5 seconds for a new attempt."
+        sleep 5
+      else
+        echo "zk0$((i + 1)).zookeeper.${ZOOKEEPER_ENV} resolved to $SERVER"
+      fi
+    done
+    unset SERVER
+  done
 fi
 
 exec "$@"
